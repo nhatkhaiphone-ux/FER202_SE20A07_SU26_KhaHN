ae80329f2d5180bab7ea4b50b61f5e987ff4c2c6
nhatkhaiphone-ux <nhatkhaiphone@gmail.com>
2026-05-13 15:57:37 +0700

    Slot2\Exercise\ex1

---
+++ Slot2/Exercise/ex1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //Khai báo một biến có tên là name và gắn giá trị "KhaHN"
+  const name = "KhaHN"
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2>KhaHN</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
